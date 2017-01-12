d64f347b61e9afc7352ea7b18c226a92f63db532
namgivu <namgivu@gmail.com>
2017-01-12 17:16:43 +0700

    fix pip install util

--- flask-app/util/prerequisite.sh
+++ flask-app/util/prerequisite.sh
@@ -8,13 +8,14 @@ echo
 #get APP_HOME path
 s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; SCRIPT_HOME=$s
 a="$SCRIPT_HOME/.." ; a=$(cd "$a" && pwd) ; APP_HOME=$a
+e="$APP_HOME/.." ; e=$(cd "$e" && pwd) ; VENV_HOME="$e/venv"
 
 echo "Steps to install prerequisite
 1.  Install venv
-    virtualenv $APP_HOME/venv
+    virtualenv $VENV_HOME
 
 2.  Activate venv
-    . $APP_HOME/venv/bin/activate
+    . $VENV_HOME/bin/activate
 
 3.  Install pip packages
     pip install -r $APP_HOME/requirements.txt
