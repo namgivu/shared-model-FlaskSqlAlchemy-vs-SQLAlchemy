b5e6d8ab5e581ab1d6e23520ce7fb99b8ef72277
namgivu <namgivu@gmail.com>
2017-01-12 18:58:43 +0700

    base model

---
+++ python-app/model/base_model.py
@@ -0,0 +1,20 @@
+from app import db
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+Base.query = db.query_property()
+
+
+class BaseModel(Base):
+  __abstract__ = True
+
+  def __repr__(self):
+    d = self.toDict()
+    return str(d)
+
+  def toDict(self):
+    '''Convert model object to Python dict type'''
+    d = {}
+    columns = self._sa_class_manager.mapper.mapped_table.columns
+    for col in columns:
+      d[col.name] = getattr(self, col.name)
+    return d
