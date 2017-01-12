4cd5bc05f0a82d922d2a6b3c53113bd718a5c208
namgivu <namgivu@gmail.com>
2017-01-12 18:04:08 +0700

    base model

---
+++ flask-app/model/base_model.py
@@ -0,0 +1,17 @@
+from app import db
+
+
+class BaseModel(db.Model):
+  __abstract__ = True
+
+  def __repr__(self):
+    d = self.toDict()
+    return str(d)
+
+  def toDict(self):
+    '''Convert model object to Python dict type'''
+    d = {}
+    columns = self._sa_class_manager.mapper.mapped_table.columns
+    for col in columns:
+      d[col.name] = getattr(self, col.name)
+    return d
