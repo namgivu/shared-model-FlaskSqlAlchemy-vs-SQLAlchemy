d177134bcbc514e0db42b8f11bae3eb0beecb215
namgivu <namgivu@gmail.com>
2017-01-13 12:45:37 +0700

    some try on python app - similar syntax works

--- python-app/application.py
+++ python-app/application.py
@@ -28,3 +28,40 @@ d = {
 
 pprint(d)
 ##endregion print user_email list
+
+##region add/delete user
+user_name = 'user333'
+
+#region add
+from model.user import User
+user = User()
+user.user_name = user_name
+
+from app import db
+db.add(user)
+db.commit()
+
+d={
+  'message': 'Added user %s' % user_name,
+  'data': user.toDict(),
+}
+pprint(d)
+#endregion add
+
+#region delete the added user
+from model.user import User
+users = User.query.filter_by(user_name=user_name).all()
+
+from app import db
+for user in users:
+  db.delete(user)
+db.commit()
+
+d={
+  'message': 'Deleted user %s' % user.user_name,
+  'data': user.toDict(),
+}
+pprint(d)
+#endregion delete the added user
+
+##endregion add/delete user
