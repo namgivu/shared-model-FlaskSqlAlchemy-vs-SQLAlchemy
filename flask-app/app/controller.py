c2c212c17275b87eb38e5b384c621604ebfbd981
namgivu <namgivu@gmail.com>
2017-01-13 11:20:41 +0700

    finish CRUD @ user

--- flask-app/app/controller.py
+++ flask-app/app/controller.py
@@ -1,5 +1,7 @@
 from app import app
+from app import db
 from flask import jsonify
+from flask import request
 
 @app.route('/')
 def index():
@@ -15,6 +17,79 @@ def user_list():
 
   d = {
     'data': users
+  }
+  return jsonify(d) #return JSON in Flask ref. http://stackoverflow.com/a/13089975/248616
+
+
+@app.route('/user/add', methods=['POST'])
+def user_add():
+  #region data from param
+  user_name = request.values.get('user_name')
+  #endregion data from param
+
+  #do adding new user
+  from model.user import User
+  user = User()
+  user.user_name = user_name
+
+  #store to db
+  db.session.add(user)
+  db.session.commit()
+
+  #output
+  d = {
+    'message': 'Added new user user_name=%s' % user_name,
+    'data': user.toDict(),
+  }
+  return jsonify(d) #return JSON in Flask ref. http://stackoverflow.com/a/13089975/248616
+
+
+@app.route('/user/delete', methods=['POST'])
+def user_delete():
+  #region data from param
+  user_id = request.values.get('user_id')
+  #endregion data from param
+
+  #get user obj from user_id
+  from model.user import User
+  user = User.query.get(user_id)
+  if not user: raise Exception('User not found user_id=%s' % user_id)
+
+  #do delete & store to db
+  db.session.delete(user)
+  db.session.commit()
+
+  #output
+  d = {
+    'message': 'Deleted user user_id=%s' % user_id,
+    'data': user.toDict(),
+  }
+  return jsonify(d) #return JSON in Flask ref. http://stackoverflow.com/a/13089975/248616
+
+
+@app.route('/user/update', methods=['POST'])
+def user_update():
+  #region data from param
+  user_id   = request.values.get('user_id')
+  user_name = request.values.get('user_name')
+  #endregion data from param
+
+  #get user obj from user_id
+  from model.user import User
+  user = User.query.get(user_id)
+  if not user: raise Exception('User not found user_id=%s' % user_id)
+
+  #do update
+  user.user_name = user_name
+
+  #store to db
+  db.session.add(user)
+  db.session.commit()
+
+  #output
+  d = {
+    'message': 'Updated user user_id=%s' % user_id,
+    'data': user.toDict(),
   }
   return jsonify(d) #return JSON in Flask ref. http://stackoverflow.com/a/13089975/248616
 ##endregion User
