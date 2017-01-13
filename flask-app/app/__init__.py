6f5240550e9dd190d99094e23f194a08074dccde
namgivu <namgivu@gmail.com>
2017-01-13 07:45:21 +0700

    move route handler to separate file

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -27,22 +27,5 @@ db.init_app(app)
 #region load db model class
 
 
-##region routing handler
-from flask import jsonify
-
-
-@app.route('/')
-def index():
-  return '122 flaask appp'
-
-
-@app.route('/user/list')
-def user_list():
-  from model.user import User
-  users = User.query.all()
-
-  d = {
-    'data': [u.toDict() for u in users]
-  }
-  return jsonify(d) #return JSON in Flask ref. http://stackoverflow.com/a/13089975/248616
-##endregion routing handler
+#routing handler
+import controller
