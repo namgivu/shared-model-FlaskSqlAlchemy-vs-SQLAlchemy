f942bf6257852936ecacc4318b67b3ea6d54d06c
namgivu <namgivu@gmail.com>
2017-01-16 14:03:20 +0700

    TODO

--- flask-app/model/user.py
+++ flask-app/model/user.py
@@ -6,10 +6,13 @@ class User(BaseModel):
   #table mapping
   __tablename__ = "users"
 
-  #column mapping
+  ##region column mapping
   id = db.Column(db.Integer, primary_key=True)
   user_name = db.Column(db.Text)
+  #primary_email_id = db.Column(db.Integer, db.ForeignKey('user_emails.id') ) #How to add the 2nd FK to UserEmail? Currently this line break the emails+owner setup
+  ##endregion column mapping
 
   ##region relationship obj
   emails = db.relationship('UserEmail', back_populates='owner')
+  #primaryEmail = db.relationship('UserEmail', foreign_keys=['primary_email_id'])
   ##endregion relationship obj
