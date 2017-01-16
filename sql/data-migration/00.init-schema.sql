9e2f497345395569e1d522daf8c631ceda5635fe
namgivu <namgivu@gmail.com>
2017-01-16 14:03:47 +0700

    user has one primary email among the registered emails @ db migrate

--- sql/data-migration/00.init-schema.sql
+++ sql/data-migration/00.init-schema.sql
@@ -6,6 +6,7 @@ use sandbox_sqlalchemy;
 CREATE TABLE `users` (
   `id`        int AUTO_INCREMENT,
   `user_name` text NOT NULL,
+  `primary_email_id` int,
   PRIMARY KEY (`id`)
 );
 
@@ -17,6 +18,10 @@ CREATE TABLE `user_emails` (
   PRIMARY KEY (`id`)
 );
 
+ALTER TABLE `users` add
+  FOREIGN KEY(primary_email_id) references user_emails(`id`)
+;
+
 set @user01Name='user1';
 set @user02Name='user22';
 insert into users(user_name) values
@@ -26,8 +31,23 @@ insert into users(user_name) values
 
 set @user01Id=(select id from users where user_name=@user01Name);
 set @user02Id=(select id from users where user_name=@user02Name);
+
+set @user1Email1='user1-email1@doomain.comm';
+set @user1Email2='user22-email1@doomain.comm';
+set @user2Email='user22-email@doomain.comm';
+
 insert into user_emails(email, user_id) values
-  ('user1-email1@doomain.comm',   @user01Id),
-  ('user1-email22@doomain.comm',  @user01Id),
-  ('user22-email@doomain.comm',   @user02Id)
+  (@user1Email1,   @user01Id),
+  (@user1Email2,   @user01Id),
+  (@user2Email,   @user02Id)
+;
+
+update `users`
+  set primary_email_id=(select id from user_emails where email=@user1Email1)
+  where id=@user01Id
+;
+
+update `users`
+  set primary_email_id=(select id from user_emails where email=@user2Email)
+  where id=@user02Id
 ;
