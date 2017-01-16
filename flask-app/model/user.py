667d3783a8bd09402b1cdbf1aaeb0f0997453dea
namgivu <namgivu@gmail.com>
2017-01-16 11:27:14 +0700

    fk TODO

--- flask-app/model/user.py
+++ flask-app/model/user.py
@@ -1,6 +1,8 @@
 from base_model import BaseModel
 from app import db
 
+from model.user_email import UserEmail
+
 
 class User(BaseModel):
   #table mapping
@@ -9,3 +11,7 @@ class User(BaseModel):
   #column mapping
   id = db.Column(db.Integer, primary_key=True)
   user_name = db.Column(db.Text)
+
+  ##region relationship obj
+  #primaryEmail = db.relationship(UserEmail, foreign_key=[User.id], remote_side=[UserEmail.id], use_list=True) #TODO How to create the `backref` directly on referred model class instead of from the referrer class?
+  ##endregion relationship obj
