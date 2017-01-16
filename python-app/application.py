e5c9da5941bb154f02a549a69a0c331533177dd5
namgivu <namgivu@gmail.com>
2017-01-16 16:31:44 +0700

    after copied model, python app STILL works

--- python-app/application.py
+++ python-app/application.py
@@ -1,6 +1,7 @@
 from app import *
 from pprint import pprint
 
+
 #region print user list
 from model.user import User
 users = User.query.all()
@@ -10,6 +11,7 @@ d = {
 }
 pprint(d)
 #endregion print user list
+
 
 ##region print user_email list
 from model.user_email import UserEmail
@@ -31,6 +33,7 @@ d = {
 
 pprint(d)
 ##endregion print user_email list
+
 
 ##region add/delete user
 user_name = 'user333'
@@ -68,3 +71,34 @@ pprint(d)
 #endregion delete the added user
 
 ##endregion add/delete user
+
+
+##region user with address
+user_name = 'uuserWithAddreess'
+
+from model.user import User
+user = User.query.filter_by(user_name=user_name).first()
+if not user: raise Exception('User not found user_name=%s' % user_name)
+
+##region raw from db
+d={
+  'message': 'User with address',
+  'data': user.toDict(),
+}
+pprint(d)
+#endregion raw from db
+
+#region followFK
+item = user.toDict()
+item['billingAddress']  = user.billingAddress.toDict()  if user.billingAddress else None
+item['shippingAddress'] = user.shippingAddress.toDict() if user.shippingAddress else None
+
+d = {
+  'message': 'User with address - followFK',
+  'data': item,
+}
+pprint(d)
+#endregion followFK
+
+pass
+##endregion user with address
